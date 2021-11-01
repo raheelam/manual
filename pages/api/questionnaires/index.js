08ac1ed1161ea67e8e7ac8d8c5dfeb9e2d4f8524
rahila Ilegbodu <raheela.ilegbodu@ozbul.com>
2021-11-01 10:33:59 +0200

    adds static prerendering

--- pages/api/questionnaires/index.js
+++ pages/api/questionnaires/index.js
@@ -1,5 +1,9 @@
 const data = require("./questionnaires.json");
 
+export function getQuestions() {
+  return data;
+}
+
 export default function getQuestionsAPI(req, res) {
   res.status(200).send(data);
 }
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ import Hero from "../components/Hero";
 import Footer from "../components/footer";
 import Services from "../components/services";
 import Quiz from "../components/Quiz";
+import { getQuestions } from "./api/questionnaires";
 
-export default function Home() {
+function Home(props) {
   const [isQuizMode, setIsQuizMode] = useState(false);
   return (
     <Layout pageTitle="Manual Landing Page">
@@ -16,7 +17,27 @@ export default function Home() {
           <Footer />
         </>
       )}
-      {isQuizMode && <Quiz setIsQuizMode={setIsQuizMode} />}
+      {isQuizMode && (
+        <Quiz setIsQuizMode={setIsQuizMode} questions={props.questions} />
+      )}
     </Layout>
   );
 }
+
+//getServerSideProps to generate during every request
+
+//to generate during build time
+export async function getStaticProps(context) {
+  try {
+    //use fetch for external endpoints
+    let resp = await getQuestions();
+    return {
+      props: { questions: resp.questions },
+      //revalidate: 25200
+    };
+  } catch (err) {
+    return { props: { questions: [] } };
+  }
+}
+
+export default Home;
